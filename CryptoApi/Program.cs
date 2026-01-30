7590caeb331bbabded16fb9e8f0e868a67d3d5b1
Susanne Rejaei <Susanne.rejaei@outlook.com>
2026-01-30 18:28:17 +0100

    add /encryption/encrypt endpoint with CaesarCipher encryption

--- CryptoApi/Program.cs
+++ CryptoApi/Program.cs
@@ -1,6 +1,13 @@
+using CryptoApi;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var cipher = new CaesarCipher();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/encryption/encrypt", (string text, int shift) =>
+{
+    return Results.Ok(cipher.Encrypt(text,shift));
+});
 app.Run();
